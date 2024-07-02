85df44e2d9aa602c3661133a910064f9fa1d7078
Glitch (wiggly-chill-shirt) <none>
2024-07-02 18:06:54 +0000

    🍡🚄 Checkpoint ./data/players.json:51770304/32 ./public/warcaby.js:51770304/162175

--- public/warcaby.js
+++ public/warcaby.js
@@ -475,9 +475,9 @@ function setup() {
     }
   }
   for (let i = 0; i < Pawns.length; i++) Pawns[i].index = i;
-  // Pawns[8].queen = true;
-  // Pawns[11].queen = true;
-  // Pawns[14].queen = true;
+  Pawns[8].queen = true;
+  Pawns[11].queen = true;
+  Pawns[14].queen = true;
 }
 let angle = 0;
 
@@ -1398,8 +1398,10 @@ function kill(blockKilledPawn, blockKillersPawn) {
               killConditionsUnique[i][3] == killConditionsUnique[j][3] &&
               killConditionsUnique[i][1] != killConditionsUnique[j][1] &&
               Pawns[killConditionsUnique[i][1]].live && Pawns[killConditionsUnique[j][1]].live &&
-              (!killConditionsUnique[j][9] && killConditionsUnique[i][10] != null) ||
-              (killConditionsUnique[j][10] != null && !killConditionsUnique[i][9])
+              (((!killConditionsUnique[j][9] && killConditionsUnique[i][10] != null) ||
+              (killConditionsUnique[j][10] != null && !killConditionsUnique[i][9])) ||
+              ((!killConditionsUnique[j][9] && killConditionsUnique[i][10] == null) ||
+              (killConditionsUnique[j][10] == null && !killConditionsUnique[i][9])))
               
               
             ) {
