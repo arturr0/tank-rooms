884d21c4ff559d336f57364345ceafe782ef3211
Glitch (wiggly-chill-shirt) <none>
2024-07-03 19:04:49 +0000

    🐛👽 Checkpoint ./public/warcaby.js:51770304/155

--- public/warcaby.js
+++ public/warcaby.js
@@ -469,7 +469,7 @@ function setup() {
       let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, true, false, true, false, false, false, Board[j].letter, Board[j].number);
       pawn.queen = true;
       Pawns.push(pawn);
-    } else if (j == 28 || j == 19 || j == 51 || j == 46 || j == 35 || j == 30) {
+    } else if (j == 10 || j == 51 || j == 46 || j == 35 || j == 30) {
       Board[j].free = false;
       let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
       Pawns.push(pawn);
@@ -915,6 +915,9 @@ let previousPlayer = null;
 let killCntr = 0;
 
 let downLeftArray = [];
+let upLeftArray = [];
+let downRightArray = [];
+let upRightArray = [];
 //fk
 function kill(blockKilledPawn, blockKillersPawn) {
 
