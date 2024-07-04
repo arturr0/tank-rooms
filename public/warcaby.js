4d5d4c457c201c5e14a8831446d6e3758529c9ed
Glitch (wiggly-chill-shirt) <none>
2024-07-04 11:18:06 +0000

    🐬🍩 Checkpoint ./public/warcaby.js:51770304/211006

--- public/warcaby.js
+++ public/warcaby.js
@@ -1775,10 +1775,10 @@ function kill(blockKilledPawn, blockKillersPawn) {
                   !killConditionsUnique[i][9] ||
                   (
                     killConditionsUnique[i][9] && 
-                    ((((killConditionsUnique[j][10] == 'up-left' && Pawns[killConditionsUnique[j][1]].row == maxLeft) ||
-                    (killConditionsUnique[j][10] == 'up-right' && Pawns[killConditionsUnique[j][1]].row == maxRight) ||
-                    (killConditionsUnique[j][10] == 'down-left' && Pawns[killConditionsUnique[j][1]].row == minLeft) ||
-                    (killConditionsUnique[j][10] == 'down-right' && Pawns[killConditionsUnique[j][1]].row == minRight)))) && 
+                    ((((killConditionsUnique[i][10] == 'up-left' && Pawns[killConditionsUnique[i][1]].row == maxLeft) ||
+                    (killConditionsUnique[i][10] == 'up-right' && Pawns[killConditionsUnique[i][1]].row == maxRight) ||
+                    (killConditionsUnique[i][10] == 'down-left' && Pawns[killConditionsUnique[i][1]].row == minLeft) ||
+                    (killConditionsUnique[i][10] == 'down-right' && Pawns[killConditionsUnique[i][1]].row == minRight)))) && 
                     !arraysEqual( 
                       Pawns[killConditionsUnique[i][0]].queensAreas.filter(area =>
                         Pawns[killConditionsUnique[i][1]].row == area[0] && Pawns[killConditionsUnique[i][1]].column == area[1]
