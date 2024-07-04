c64c07eaacdf9f71718da5097d87878271842552
Glitch (wiggly-chill-shirt) <none>
2024-07-04 12:28:59 +0000

    🏦🌝 Checkpoint ./public/warcaby.js:51770304/8

--- public/warcaby.js
+++ public/warcaby.js
@@ -464,13 +464,13 @@ function setup() {
   }
   
   for (let j = 0; j < Board.length; j++) {
-    if (j == 37) {
+    if (j == 19) {
       Board[j].free = false;
       let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, true, false, true, false, false, false, Board[j].letter, Board[j].number);
       pawn.queen = true;
       Pawns.push(pawn);
       generateQueensAreas();
-    } else if (j == 28 || j == 10) {
+    } else if (j == 28 || j == 46) {
       Board[j].free = false;
       let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
       Pawns.push(pawn);
