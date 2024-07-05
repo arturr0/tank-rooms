3bf3959617f280747ee987850dec13caee6093f4
Glitch (wiggly-chill-shirt) <none>
2024-07-05 19:54:56 +0000

    🌆🌅 Checkpoint ./public/warcaby.js:51770304/58

--- public/warcaby.js
+++ public/warcaby.js
@@ -148,8 +148,8 @@ function Pawn(rectCenter, rectCenterY, row, column, isRed, queen, live, killer, 
     this.update = function() {
       if (this.targetPos) {
         let vel = p5.Vector.sub(this.targetPos, this.pos);
-        if (vel.mag() > 10 && this.live) {
-          vel.setMag(10);
+        if (vel.mag() > 1 && this.live) {
+          vel.setMag(1);
           this.pos.add(vel);
           pawnCompletedMove = false;
         } else {
@@ -461,13 +461,13 @@ function setup() {
     //f(rectCenter, rectCenterY, row, column, isRed, queen, live, killer, killed, letter, number)
     for (let j = 0; j < Board.length; j++) {
       // if (Board[j].isBlack && Board[j].row < 4) {
-      if ([55].includes(j)) {
+      if ([56].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, true, false, true, false, false, false, Board[j].letter, Board[j].number);
         pawn.queen = true;
         Pawns.push(pawn);
       // } else if (Board[j].isBlack && Board[j].row > 5) {
-      } else if ([46, 28, 10].includes(j)) {
+      } else if ([49, 35, 21].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
         Pawns.push(pawn);
