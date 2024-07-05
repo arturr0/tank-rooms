01e7601e503c57173d19d12d9f32118da273de2a
Glitch (wiggly-chill-shirt) <none>
2024-07-05 19:09:26 +0000

    🙉💳 Checkpoint ./public/warcaby.js:51770304/192478

--- public/warcaby.js
+++ public/warcaby.js
@@ -460,14 +460,14 @@ function setup() {
     }
     //f(rectCenter, rectCenterY, row, column, isRed, queen, live, killer, killed, letter, number)
     for (let j = 0; j < Board.length; j++) {
-      // if (Board[j].isBlack && Board[j].row < 4) {
-      if ([1, 3, 5].includes(j)) {
+      if (Board[j].isBlack && Board[j].row < 4) {
+      //if ([1, 3, 5].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, true, false, true, false, false, false, Board[j].letter, Board[j].number);
-        pawn.queen = true;
+        //pawn.queen = true;
         Pawns.push(pawn);
-      // } else if (Board[j].isBlack && Board[j].row > 5) {
-      } else if ([10, 12, 14, 26, 28].includes(j)) {
+      } else if (Board[j].isBlack && Board[j].row > 5) {
+      //} else if ([10, 12, 14, 26, 28].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
         Pawns.push(pawn);
