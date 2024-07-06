e100c7ce83c44a13e65ce6c52c0c82e9be397254
Glitch (wiggly-chill-shirt) <none>
2024-07-06 13:50:59 +0000

    🚐🏍 Checkpoint ./public/warcaby.js:51770304/781903

--- public/warcaby.js
+++ public/warcaby.js
@@ -461,13 +461,14 @@ function setup() {
     //f(rectCenter, rectCenterY, row, column, isRed, queen, live, killer, killed, letter, number)
     for (let j = 0; j < Board.length; j++) {
       // if (Board[j].isBlack && Board[j].row < 4) {
-      if ([1, 3, 5].includes(j)) {
+      if ([21].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, true, false, true, false, false, false, Board[j].letter, Board[j].number);
         pawn.queen = true;
         Pawns.push(pawn);
+        generateQueensAreas()
       // } else if (Board[j].isBlack && Board[j].row > 5) {
-      } else if ([10, 12, 14, 26, 30, 28].includes(j)) {
+      } else if ([19, 37, 12].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
         Pawns.push(pawn);
@@ -1591,14 +1592,14 @@ console.log("Min Right:", minRight);
                 !killConditionsUnique[i][9] ||
                 (
                   killConditionsUnique[i][9] && killConditionsUnique[j][9] && 
-                  // ((((killConditionsUnique[i][10] == 'up-left' && maxLeft.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row)) ||
-                  // (killConditionsUnique[i][10] == 'up-right' && maxRight.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row)) ||
-                  // (killConditionsUnique[i][10] == 'down-left' && minLeft.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row)) ||
-                  // (killConditionsUnique[i][10] == 'down-right' && minRight.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row))))) &&
-                  // ((((killConditionsUnique[j][10] == 'up-left' && maxLeft.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row)) ||
-                  // (killConditionsUnique[j][10] == 'up-right' && maxRight.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row)) ||
-                  // (killConditionsUnique[j][10] == 'down-left' && minLeft.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row)) ||
-                  // (killConditionsUnique[j][10] == 'down-right' && minRight.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row))))) && 
+                  ((((killConditionsUnique[i][10] == 'up-left' && maxLeft.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row)) ||
+                  (killConditionsUnique[i][10] == 'up-right' && maxRight.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row)) ||
+                  (killConditionsUnique[i][10] == 'down-left' && minLeft.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row)) ||
+                  (killConditionsUnique[i][10] == 'down-right' && minRight.some(array => array[1] === Pawns[killConditionsUnique[i][1]].row))))) &&
+                  ((((killConditionsUnique[j][10] == 'up-left' && maxLeft.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row)) ||
+                  (killConditionsUnique[j][10] == 'up-right' && maxRight.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row)) ||
+                  (killConditionsUnique[j][10] == 'down-left' && minLeft.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row)) ||
+                  (killConditionsUnique[j][10] == 'down-right' && minRight.some(array => array[1] === Pawns[killConditionsUnique[j][1]].row))))) && 
                   !arraysEqual( 
                     Pawns[killConditionsUnique[i][0]].queensAreas.filter(area =>
                       Pawns[killConditionsUnique[i][1]].row == area[0] && Pawns[killConditionsUnique[i][1]].column == area[1]
@@ -1769,7 +1770,7 @@ function stepKill(killmode) {
 
 
 for (let i = 0; i < killmode.length; i++)
-if (((Player == 1 && !Greenturn) || (Player == 2 && Greenturn)) && (!killersOptMode && !killedOptMode && (!oneKiller2Killed || (oneKiller2Killed && step == 0))) && !Pawns[killmode[i][1]].live)   
+if (((Player == 1 && !Greenturn) || (Player == 2 && Greenturn)) && !killersOptMode && !killedOptMode && !Pawns[killmode[i][1]].live)   
   //if (Pawns[killmode[i][0]].live && !Pawns[killmode[i][1]].live ) 
 { 
   step++;
