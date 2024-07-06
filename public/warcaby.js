8f72dd2f19464faccad63356e72dc683fd9da688
Glitch (wiggly-chill-shirt) <none>
2024-07-06 21:49:44 +0000

    🗼🍁 Checkpoint ./public/warcaby.js:51770304/589650

--- public/warcaby.js
+++ public/warcaby.js
@@ -468,7 +468,7 @@ function setup() {
         Pawns.push(pawn);
         generateQueensAreas()
       // } else if (Board[j].isBlack && Board[j].row > 5) {
-      } else if ([1,14,28,42].includes(j)) {
+      } else if ([1,14,21,35].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
         Pawns.push(pawn);
@@ -1173,15 +1173,18 @@ function kill(blockKilledPawn, blockKillersPawn) {
           
           for (let i = 0; i < Board.length; i++)
             for (let j = 0; j < downLeftArray.length; j++)
-              if (Board[i].row - Pawns[downLeftArray[j][1]].row == -1 && Board[i].column - Pawns[downLeftArray[j][1]].column == 1)
-                {
-                  let cond = downLeftArray.some(yourPawn => 
-                    Pawns[downLeftArray[j][1]].isRed == Pawns[yourPawn[1]].isRed
-                    && Pawns[yourPawn[1]].live &&
-                    Board[i].column == Pawns[yourPawn[1]].column && Board[i].row == Pawns[yourPawn[1]].row
-                  )
-                  console.log("cond", cond)
-                } 
+              
+              if (Board[i].row - Pawns[downLeftArray[j][1]].row == -1 && Board[i].column - Pawns[downLeftArray[j][1]].column == 1 
+                &&
+                !downLeftArray.every(yourPawn => { 
+                Pawns[downLeftArray[j][1]].isRed == Pawns[yourPawn[1]].isRed
+                && Pawns[yourPawn[1]].live &&
+                Board[i].column == Pawns[yourPawn[1]].column && Board[i].row == Pawns[yourPawn[1]].row
+                console.log("loop", yourPawn)
+                }
+                )
+              )
+              { 
                 Board[i].check = true;
                 console.log("check behind killed postions", downLeftArray[j][0], downLeftArray[j][1], downLeftArray[j][2]);
                 //Board[i].check = true;
@@ -1202,7 +1205,7 @@ function kill(blockKilledPawn, blockKillersPawn) {
                 // let maxLeft = Math.max(...killConditionsUnique.filter(subarray => subarray[10] == 'down-left').map(subarray => Pawns[subarray[1]].row));
                 // console.log(maxLeft)
                 break;
-            
+            }
             break;
           //killConditions.push([k, j, i, Pawns[k].isRed, Greenturn, Pawns[k].rectCenter, Pawns[k].rectCenterY, Pawns[j].rectCenter, Pawns[j].rectCenterY, true, null]);      
           // for (let j = 0; j < killConditions.length; j++) {
