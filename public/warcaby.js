1d3d0f5f518508471249e3a48bab410b7d9ca3e7
Glitch (wiggly-chill-shirt) <none>
2024-07-07 13:14:59 +0000

    🐀👲 Checkpoint ./public/warcaby.js:51770304/801043

--- public/warcaby.js
+++ public/warcaby.js
@@ -468,7 +468,7 @@ function setup() {
         Pawns.push(pawn);
         generateQueensAreas()
       // } else if (Board[j].isBlack && Board[j].row > 5) {
-      } else if ([1,14,28,42].includes(j)) {
+      } else if ([1,14,21,35].includes(j)) {
         Board[j].free = false;
         let pawn = new Pawn(Board[j].rectCenter, (Board[j].row * 64 - 32) + 32, Board[j].row, Board[j].column, false, false, true, false, false, false, Board[j].letter, Board[j].number);
         Pawns.push(pawn);
@@ -1166,7 +1166,7 @@ function kill(blockKilledPawn, blockKillersPawn) {
           // for (let i = 0; i < downLeftArray.length; i++) {
           //   console.log("push downLeftArray", downLeftArray[i]);
           // }
-          
+          let free;
           for (let i = 0; i < Board.length; i++)
             for (let j = 0; j < downLeftArray.length; j++)
               if (Board[i].row - Pawns[downLeftArray[j][1]].row == -1 && Board[i].column - Pawns[downLeftArray[j][1]].column == 1 
@@ -1178,8 +1178,14 @@ function kill(blockKilledPawn, blockKillersPawn) {
                 
                 )
               )
-              console.log("free")  // &&
-                
+              //console.log("free")  // &&
+              {
+                free = false;
+                //downLeftArray = []
+                console.log("free")
+              }
+              
+                 
                 // // Board.some(board =>
                 // //   board.free && board.queen &&
                 // //   Pawns[killed].row - board.row == -1 &&
@@ -1213,9 +1219,12 @@ function kill(blockKilledPawn, blockKillersPawn) {
                 //let nearest = Math.min(...rows);
                 //console.log("nearest", nearest)
                 // if(checkedBoard == numberOfKilled) {
-                // killConditions.push([downLeftArray[j][0], downLeftArray[j][1], downLeftArray[j][2], Pawns[downLeftArray[j][0]].isRed, Greenturn, Pawns[downLeftArray[j][0]].rectCenter, Pawns[downLeftArray[j][0]].rectCenterY, Pawns[downLeftArray[j][1]].rectCenter, Pawns[downLeftArray[j][1]].rectCenterY, true, 'down-left']);
-                // killConditionsUnique = JSON.parse(JSON.stringify(killUnique(killConditions)));
-                // }
+                if(free) {
+                  for (let j = 0; j < downLeftArray.length; j++) {
+                    killConditions.push([downLeftArray[j][0], downLeftArray[j][1], downLeftArray[j][2], Pawns[downLeftArray[j][0]].isRed, Greenturn, Pawns[downLeftArray[j][0]].rectCenter, Pawns[downLeftArray[j][0]].rectCenterY, Pawns[downLeftArray[j][1]].rectCenter, Pawns[downLeftArray[j][1]].rectCenterY, true, 'down-left']);
+                    killConditionsUnique = JSON.parse(JSON.stringify(killUnique(killConditions)));
+                  }
+                }// }
                 // Board.forEach(board => {
                 //   if (board.check) {
                 //     board.check = false; // Update check to false
