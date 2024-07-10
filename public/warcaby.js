1f2b511391949f905fb25261aef38c241e7ae1e5
Glitch (wiggly-chill-shirt) <none>
2024-07-10 09:10:21 +0000

    🎺🐴 Checkpoint ./public/warcaby.js:51770304/428469

--- public/warcaby.js
+++ public/warcaby.js
@@ -122,6 +122,7 @@ let releaseBlock = false;
 //let killerSelected = null;
 let queenKillConditions = [];
 let uniqueQueenKillConditions = [];
+let chooseBoard = [];
 
 let message;
 // let pawnLetter;
@@ -1364,8 +1365,8 @@ for (let i = 0; i < Board.length; i++) {
       if (((blockKilledPawn === null && blockKillersPawn === null) || (blockKilledPawn === k || blockKillersPawn === k)) &&
           Pawns[j].isRed != Pawns[k].isRed && Pawns[j].live && Pawns[k].live && Pawns[k].queen &&
         ((Player == 1 && Greenturn == false && Pawns[j].isRed == false) || (Player == 2 && Greenturn == true && Pawns[j].isRed == true)) &&
-          Board[i].queen && Pawns[j].row - Board[i].row >= 1 &&
-          Pawns[j].column - Board[i].column <= -1 && Board[i].row < Pawns[j].row && Board[i].free &&
+          Board[i].queen && (Pawns[j].row - Board[i].row >= 1 || Pawns[j].rowCopy - Board[i].row >= 1) &&
+          (Pawns[j].column - Board[i].column <= -1 || Pawns[j].columnCopy - Board[i].column <= -1) && Board[i].row < Pawns[j].row && Board[i].free &&
           Pawns[k].queensAreas.some(area => 
             area[2] === 'up-right' &&
             Pawns[j].row === area[0] &&
@@ -1412,8 +1413,8 @@ for (let i = 0; i < Board.length; i++) {
             if (((blockKilledPawn === null && blockKillersPawn === null) || (blockKilledPawn === k || blockKillersPawn === k)) &&
                Pawns[j].isRed != Pawns[k].isRed && Pawns[j].live && Pawns[k].live && Pawns[k].queen &&
               ((Player == 1 && Greenturn == false && Pawns[j].isRed == false) || (Player == 2 && Greenturn == true && Pawns[j].isRed == true)) &&
-                Board[i].queen && Pawns[j].row - Board[i].row <= -1 &&
-                Pawns[j].column - Board[i].column <= -1 && Board[i].row > Pawns[j].row &&
+                Board[i].queen && (Pawns[j].row - Board[i].row <= -1 || Pawns[j].rowCopy - Board[i].row <= -1) &&
+                (Pawns[j].column - Board[i].column <= -1 || Pawns[j].columnCopy - Board[i].column <= -1) && Board[i].row > Pawns[j].row &&
                 Pawns[k].queensAreas.some(area => 
                   area[2] === 'down-right' &&
                   Pawns[j].row === area[0] &&
@@ -1443,6 +1444,8 @@ for (let i = 0; i < Board.length; i++) {
               
           }
       }
+
+      //fq
       console.log("check array");
       downRightArray = groupAndSort("down", downRightArray);
       neighbourFilter("down", downRightArray, -1, -1);
