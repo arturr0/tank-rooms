dcf61e94b0326a318d0612ffcce4044028a498c4
Glitch (wiggly-chill-shirt) <none>
2024-07-14 09:46:49 +0000

    🏉⏲ Checkpoint ./public/warcaby.js:51770304/1310304

--- public/warcaby.js
+++ public/warcaby.js
@@ -2073,9 +2073,7 @@ function killSwitch(winner, looser, newBoard, player, chooseBoard) {
         console.log(chooseBoard);
       for (let i = 0; i < chooseBoard.length; i++)  
         if(Pawns[chooseBoard[i][1]].liveCopy.length == 0 || 
-        !Pawns[chooseBoard[i][1]].liveCopy.some(array => array[0] === chooseBoard[i][0] &&
-        array[1] != chooseBoard[i][10]
-        )) {
+        Pawns[chooseBoard[i][1]].liveCopy.some(array => array[0] === chooseBoard[i][0] && array[1] == chooseBoard[i][10])) {
         Pawns[chooseBoard[i][1]].liveCopy.push([chooseBoard[i][0], chooseBoard[i][10]]);
         console.log("ks in", winner, looser, newBoard);
         Pawns[chooseBoard[i][0]].rowCopy = Board[chooseBoard[i][2]].row;
